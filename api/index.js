d662b897f95aa170309b09d90cf0df28a84459d9
“[Rahul <rockybhai3928@gmail.com>
2024-03-08 08:46:36 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -3,17 +3,17 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connectToDataBase = () => {
-    mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log("Connected To DB Sucessfully....");
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-}
+mongoose.connect("mongodb+srv://RahulKumavat:Rahul@shelter-estate.khqboix.mongodb.net/?retryWrites=true&w=majority&appName=SHELTER-ESTATE")
+.then(() => {
+    console.log('connected to MongoDB!');
+})
+.catch((err) => {
+    console.log(err);
+});
+
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 5000!');
-  });  
+app.listen(5000, () => {
+    console.log('server is running on port 5000!');
+    }
+)
