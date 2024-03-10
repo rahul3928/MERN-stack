e3436c5658d848e0ee7fc74d6c34e7ec4716aaf3
“[Rahul <rockybhai3928@gmail.com>
2024-03-10 09:50:29 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect("mongodb+srv://RahulKumavat:Rahul@shelter-estate.khqboix.mongodb.net/?retryWrites=true&w=majority&appName=SHELTER-ESTATE")
@@ -13,9 +14,11 @@ mongoose.connect("mongodb+srv://RahulKumavat:Rahul@shelter-estate.khqboix.mongod
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(5000, () => {
     console.log('server is running on port 5000!');
     }
 );
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
